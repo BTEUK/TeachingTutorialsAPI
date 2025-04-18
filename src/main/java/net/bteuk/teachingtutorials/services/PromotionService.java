6c20d2a708ab1539c3c443ad33a2a2f37acaf566
ELgamer <laurens.wolfert@gmail.com>
2025-04-18 15:14:32 +0200

    Revert "API without Bukkit"
    This reverts commit fa5ca1ffffc2b1e7ea72053c873f9619e5f8c5d0.

--- src/main/java/net/bteuk/teachingtutorials/services/PromotionService.java
+++ src/main/java/net/bteuk/teachingtutorials/services/PromotionService.java
@@ -1,6 +1,6 @@
 package net.bteuk.teachingtutorials.services;
 
-import java.util.UUID;
+import org.bukkit.entity.Player;
 
 /**
  * Handles promotion of a player after completion of the compulsory tutorial
@@ -9,10 +9,9 @@ public interface PromotionService {
 
     /**
      * Calls the promotion logic for the promotion of the given player upon completion of the compulsory tutorial
-     * @param playerUuid uuid of the player to promote
-     * @param playerName name of the player to promote
+     * @param player The Bukkit Player object of the player to promote
      */
-    void promote(UUID playerUuid, String playerName);
+    void promote(Player player);
 
     /**
      *
